92570d079320ef91ac7b76a959d15fd209af03e8
maddietenace <mten0007@student.monash.edu>
2025-06-06 15:03:01 +1000

    upload

--- class-changes.js
+++ class-changes.js
@@ -2,17 +2,47 @@ const classChanges = [
   { time: 0, target: ".pop5", add: "active" },
   { time: 5, target: ".pop5", remove: "active" },
 
-  { time: 3, target: ".mainpage", add: "active" },
-  { time: 5, target: ".mainpage", remove: "active" },
+  { time: 34, target: ".pop6", add: "active" },
+  { time: 39, target: ".pop6", remove: "active" },
+
+  { time: 34, target: ".mainpage", add: "active" },
+  { time: 39, target: ".mainpage", remove: "active" },
+
+  { time: 58, target: ".pop6", add: "active" },
+  { time: 60, target: ".pop6", remove: "active" },
+
+  { time: 58, target: ".mainpage", add: "active" },
+  { time: 60, target: ".mainpage", remove: "active" },
 
   { time: 63, target: ".pop2", add: "active" },
   { time: 72, target: ".pop2", remove: "active" },
 
+  { time: 81, target: ".pop6", add: "active" },
+  { time: 84, target: ".pop6", remove: "active" },
+
+  { time: 81, target: ".mainpage", add: "active" },
+  { time: 84, target: ".mainpage", remove: "active" },
+
   { time: 85, target: ".pop1", add: "active" },
   { time: 95, target: ".pop1", remove: "active" },
 
+  { time: 105, target: ".pop6", add: "active" },
+  { time: 112, target: ".pop6", remove: "active" },
+
+  { time: 105, target: ".mainpage", add: "active" },
+  { time: 112, target: ".mainpage", remove: "active" },
+
+  { time: 124, target: ".pop6", add: "active" },
+  { time: 128, target: ".pop6", remove: "active" },
+
+  { time: 124, target: ".mainpage", add: "active" },
+  { time: 128, target: ".mainpage", remove: "active" },
+
   { time: 129, target: ".pop2", add: "active" },
   { time: 139, target: ".pop2", remove: "active" },
+
+  { time: 145, target: ".pop7", add: "active" },
+  { time: 150, target: ".pop7", remove: "active" },
 
   { time: 212, target: ".pop1", add: "active" },
   { time: 219, target: ".pop1", remove: "active" },
@@ -26,14 +56,29 @@ const classChanges = [
   { time: 298, target: ".pop1", add: "active" },
   { time: 306, target: ".pop1", remove: "active" },
 
+  { time: 300, target: ".pop8", add: "active" },
+  { time: 305, target: ".pop8", remove: "active" },
+
   { time: 314, target: ".pop2", add: "active" },
   { time: 322, target: ".pop2", remove: "active" },
 
   { time: 338, target: ".pop1", add: "active" },
   { time: 343, target: ".pop1", remove: "active" },
 
+  { time: 354, target: ".pop6", add: "active" },
+  { time: 357, target: ".pop6", remove: "active" },
+
+  { time: 354, target: ".mainpage", add: "active" },
+  { time: 357, target: ".mainpage", remove: "active" },
+
   { time: 358, target: ".pop1", add: "active" },
   { time: 361, target: ".pop1", remove: "active" },
+
+  { time: 393, target: ".pop6", add: "active" },
+  { time: 397, target: ".pop6", remove: "active" },
+
+  { time: 393, target: ".mainpage", add: "active" },
+  { time: 397, target: ".mainpage", remove: "active" },
 
   { time: 398, target: ".pop2", add: "active" },
   { time: 405, target: ".pop2", remove: "active" },
